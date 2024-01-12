b72da7cf822649f9e6e82f9fcfcffb9f066519ca
Shane Deocales <126802691+ShaneyDee@users.noreply.github.com>
2024-01-12 05:07:56 +0000

    Palindrome

--- Main.java
+++ Main.java
@@ -1,1 +1,25 @@
+import java.util.*;
 
+class Main {
+
+    public static void main (String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a word: ");
+        String str = sc.nextLine();
+
+        StringBuilder strb = new StringBuilder(str);
+        strb = strb.reverse();
+
+        String rev = strb.toString();
+
+        if(str.equals(rev)){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
+    }
+        
+}
